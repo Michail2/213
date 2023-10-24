af1831022d2665f1e64f62963ffa45ce9dd2a8d3
Your Name <you@example.com>
2023-10-24 15:07:23 +0300

    1

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict"
+let a = 0;
+console.log( 123 );
+let name = prompt( "Ваше имя?" );
+document.write( "<br>" + name + "</br>" );
+
+/* Тринарный оператор  let adult = age >= 18 ? true: false; = if (age >= 18) { adult = true;} else { adult = false; }*/
+/* Функция let ok = confirm('Текст вопроса'); вызывает окно с вопросом и кнопками "ок" и "отмена", "ок" возвращает "true", а "отмена" возвращает "false" */
+
